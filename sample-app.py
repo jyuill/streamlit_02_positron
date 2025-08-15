c41ef67946ccb61bed0b6bccd89b8e5c690cfeec
John Yuill <john@fig4.com>
2025-08-14 20:55:35 -0700

    trying to get something going here

--- sample-app.py
+++ sample-app.py
@@ -16,8 +16,41 @@ with st.sidebar:
 
 @st.cache_data
 def get_data(st_filter):
-    state_tracts = tracts(st_filter, ch=True, year = 2021, cache = True).to_crs(6571)
+    state_tracts = tracts(st_filter, cb=True, 
+                            year = 2021, 
+                            cache = True).to_crs(6571)
     state_tracts = state_tracts.to_crs('EPSG:4326')
-    return state_tracts
+    return(state_tracts)
 
 state_tracts = get_data(state)
+
+@st.cache_data
+def load_data(state):
+    trauma = gp.read_file('trauma.geojson')
+    trauma = trauma[trauma['STATE'] == state]
+    return(trauma)
+
+trauma = load_data(state)
+
+col1, col2 = st.columns(2)
+
+with st.container():
+    with col1:
+        st.subheader('Location of Trauma Hospitals')
+        st.map(trauma)
+
+    with col2:
+        st.subheader('Raw data')
+        st.write(trauma[['NAME', 'ADDRESS', 'CITY', 'STATE', 'ZIP']])
+        state_tracts = state_tracts.to_crs(6571)
+        trauma = trauma.to_crs(6571)
+        state_buffer = gp.GeoDataFrame(geometry=state_tracts.dissolve().buffer(100000))
+        state_trauma = gp.sjoin(trauma, state_buffer, how='inner')
+        tract_centroids = state_tracts.centroid
+        dist = tract_centroids.geometry.apply(lambda g= state_trauma.distance(g, align = False))
+        min_dist = dist.min(axis = 'columns') / 1000
+        hist_values = np.histogram(min_dist, bins=24, range = (0,24))[0]
+
+with st.container():
+    st.subheader('Min distance to trauma center from tract centroid')
+    st.barchart(hist_values)
